21ca79abfcaf868b5c0b6cf93ca0f3e30d079e2f
deathstroke <43871004+harish-ganesh@users.noreply.github.com>
2020-03-12 13:11:27 +0530

    Added precision and recall

--- fcm.R
+++ fcm.R
@@ -1,8 +1,9 @@
 require(ppclust)
-require(factoextra)
+#require(factoextra)
 require(dplyr)
 require(cluster)
 require(fclust)
+library(caret)
 data("iris3")
 x=iris[,-5]
 
@@ -11,4 +12,13 @@ cor(iris[,1:4])
 #require(psych)
 #pairs.panels(iris[,-5], method = "pearson")
 res.fcm <- fcm(x, centers=3)
-summary(res.fcm)
+as.data.frame(res.fcm$u)[1:6,]
+#summary(res.fcm)
+iris[,6] = res.fcm$cluster
+df = as.data.frame(iris)
+df[which(df[6]==1),6] = "versicolor"
+df[which(df[6]==2),6] = "setosa"
+df[which(df[6]==3),6] = "virginica"
+print(precision(table(df[,5],df[,6]),relevant = "setosa"))
+confusionMatrix(table(df[,5],df[,6]))
+F_meas(table(df[,5],df[,6]),relevant = "virginica")
